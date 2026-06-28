941e86dc7a8ccd3b8bd4bfc159f1df5266b4f0d7
cocot-gf <gadget_factory@bf.wakwak.com>
2026-06-28 19:24:29 +0900

    Add files via upload

---
+++ ParaSol_1.0_src/src/include/spiTransfer.h
@@ -0,0 +1,13 @@
+/* ParaSol
+ * spiTransfer.h
+ *
+ *  Created on: 2026/06/07
+ *      Author: cocot
+ */
+
+#ifndef SRC_INCLUDE_SPITRANSFER_H_
+#define SRC_INCLUDE_SPITRANSFER_H_
+
+void beginTransfer(void);
+
+#endif /* SRC_INCLUDE_SPITRANSFER_H_ */
